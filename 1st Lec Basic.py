a4a4a19b1bfe7e0436acaf7524eda5876cb53338
Padariya-Pranav <pranavpadariya007@gmail.com>
2026-01-12 15:10:31 +0530

    Add files via upload

---
+++ 1st Lec Basic.py
@@ -0,0 +1,16 @@
+def add():
+
+    a=  int (input("Enter Value For A:"))
+    b= float (input("Enter Value For B:"))
+    c = a + b
+    print(a, "+" ,b ,"=", c )
+
+        #print
+    print("Answer =", c)
+    print("Answer = ",c , a, sep = " PRANAV "  , end='\t')
+    print("This is the last line.")
+
+     
+
+add()
+
---
+++ ADD 2 NUM.py
@@ -0,0 +1,10 @@
+
+def add():
+    a= int (input("Enter A:"))
+    b= int (input("Enter B:"))
+    c = a+b
+
+    print("ANSWER :" ,a, "+" , b , "=", c , sep='  ')
+
+    
+add()
